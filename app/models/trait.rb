b4f0e9eee6abf4722b556d82d56835d205c0a03b
Alex Rudall <alex@sardjv.co.uk>
2014-11-01 23:10:14 +0000

    Refactor Novel show & index page HTML

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -1,5 +1,5 @@
 class Trait < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :description
   validates_uniqueness_of :name, scope: :character_id
   
   belongs_to :character
